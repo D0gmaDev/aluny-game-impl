ea212cbd6800c56e7462cfeb578ce1dd9b79cb73
D0gma_ <d0gma@d0gma.fr>
2023-06-11 11:38:47 +0200

    make VersionMatcherImpl fail softly

--- src/main/java/fr/aluny/gameimpl/version/VersionMatcherImpl.java
+++ src/main/java/fr/aluny/gameimpl/version/VersionMatcherImpl.java
@@ -9,17 +9,24 @@ import org.bukkit.Bukkit;
 
 public class VersionMatcherImpl {
 
-    private static final String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
+    private static final String         SERVER_VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
+    private static final VersionWrapper DUMMY_WRAPPER  = new DummyWrapper();
 
-    private static final Map<String, Supplier<? extends VersionWrapper>> versions = Map.of("1_19_R1", Wrapper_1_19_R1::new);
+    private static final Map<String, Supplier<VersionWrapper>> VERSIONS = Map.of("1_19_R3", Wrapper_1_19_R3::new);
+
 
     public static void matchVersion() {
         VersionMatcher.setWrapper(match());
     }
 
     private static VersionWrapper match() {
-        VersionWrapper versionWrapper = Optional.ofNullable(versions.get(serverVersion)).orElseThrow(() -> new IllegalStateException("[VERSION] VersionMatcher cannot be found: " + serverVersion)).get();
-        Bukkit.getLogger().info("[VERSION] VersionWrapper " + versionWrapper.getClass().getSimpleName() + " found.");
-        return versionWrapper;
+        Optional<Supplier<VersionWrapper>> versionMatcher = Optional.ofNullable(VERSIONS.get(SERVER_VERSION));
+
+        versionMatcher.ifPresentOrElse(
+                versionWrapper -> Bukkit.getLogger().info("[VERSION] VersionWrapper " + versionWrapper.getClass().getSimpleName() + " found."),
+                () -> Bukkit.getLogger().warning(("[VERSION] VersionMatcher cannot be found for version: " + SERVER_VERSION + ". Using DummyWrapper instead, server may not work as intended."))
+        );
+
+        return versionMatcher.map(Supplier::get).orElse(DUMMY_WRAPPER);
     }
 }
